a917ff38639a398cdcb06f353f97d1de2c1c6b68
Jan Proczek <janproczek@gmail.pl>
2023-02-15 23:51:40 +0100

    Enable rocket to move, add bullet

--- settings.py
+++ settings.py
@@ -7,3 +7,13 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # Rocket settings
+        self.ship_speed = 10
+
+        # Bullet settings
+        self.bullet_speed = 12.0
+        self. bullet_width = 10
+        self.bullet_height = 10
+        self.bullet_color =(60, 60, 60)
+        self.bullets_allowed = 3
